a6706c8bf57c0ed520273b01b92a3ca8f8f6c85d
mathisnozais <72189821+mathisnozais@users.noreply.github.com>
2025-06-26 16:12:39 +0200

    Add files via upload

--- 01_BulkRNA_preprocessing/02_Script/script_RNA_preprocess_GIT.sh
+++ 01_BulkRNA_preprocessing/02_Script/script_RNA_preprocess_GIT.sh
@@ -7,23 +7,18 @@ NUM_PROC_SMALL=$((NUM_PROC / 3)) #For tools that are limited by RAM capacity, ca
 
 ########################################## STEP 0 #######################################################
 ####### Modify Variable ######
-WORKING_DIR=$(eval echo \$$1) # path to the cloned github declare when launching the script
+WORKING_DIR=$(eval echo \$$1) # path to the cloned github
 rawdata="$WORKING_DIR/01_BulkRNA_preprocessing/01_RawData" #folder with rawdata
 
 unique_names=$(find "$rawdata" -type f -name "*.fastq.gz" | sed 's|.*/||')
-unique_names=$(echo "$unique_names" | sed 's/_.*//')
-
-mkdir -p $WORKING_DIR/01_BulkRNA_preprocessing/03_Preprocessed/Trimming
-mkdir -p $WORKING_DIR/01_BulkRNA_preprocessing/03_Preprocessed/Mapping
-mkdir -p $WORKING_DIR/01_BulkRNA_preprocessing/03_Preprocessed/Count
-
+unique_names=$(echo "$unique_names" | sed 's/_.*//'|sort|uniq)
 
 ########################################## STEP 1 #######################################################
 ####### Check quality and trimming ######
 
 for name in $unique_names; do
 	echo "FastQC $name"
-	FILE=$rawdata/${name}_1.fastq.gz #checking if the output of the tool is existing
+	FILE=$rawdata/${name}_1.fastqc.zip #checking if the output of the tool is existing
 	if [ -f "$FILE" ]; then
 		echo "$FILE exists - Skipping FastqC"
 	else
@@ -35,7 +30,7 @@ done
 multiqc $rawdata -o $rawdata
 
 # Trimming
-cp /home/nozais/miniconda3/envs/RNAbulk/share/trimmomatic-0.39-2/adapters/NexteraPE-PE.fa .
+cp /home/$USER/miniconda3/envs/RNAbulk/share/trimmomatic-0.39-2/adapters/NexteraPE-PE.fa .
 for name in $unique_names; do
 	echo "Trimming $name"
 	FILE=$workdir/02_Preprocessed/Trimming/${name}_1.trimmed.fastq #checking if the output of the tool is existing
@@ -66,7 +61,7 @@ for name in $unique_names; do
   if [ -f "$FILE" ]; then
   	echo "$FILE exists - Skipping Hisat2"
   else
-  	hisat2 -x $workdir/01_BulkRNA_preprocessing/Reference/human_index_Hisat -1 $workdir/02_Preprocessed/Trimming/${name}_1.trimmed.fastq -2 $workdir/02_Preprocessed/Trimming/${name}_2.trimmed.fastq --summary-file $workdir/02_Preprocessed/Mapping/${name}_summary.txt -S $workdir/02_Preprocessed/Mapping/${name}.sam -p $NUM_PROC_SMALL
+  	hisat2 -x /mnt/NASBIOINFO_MN/LALT/BIOINFO/References/Human/Hg38_P14/human_index_Hisat -1 $workdir/02_Preprocessed/Trimming/${name}_1.trimmed.fastq -2 $workdir/02_Preprocessed/Trimming/${name}_2.trimmed.fastq --summary-file $workdir/02_Preprocessed/Mapping/${name}_summary.txt -S $workdir/02_Preprocessed/Mapping/${name}.sam -p $NUM_PROC_SMALL
   fi
   
   FILE=$workdir/02_Preprocessed/Mapping/${name}.bam
@@ -80,12 +75,12 @@ for name in $unique_names; do
   if [ -f "$FILE" ]; then
   	echo "$FILE exists - Skipping Samtools sort"
   else
-  	samtools sort $workdir/02_Preprocessed/Mapping/${name}.bam > $workdir/02_Preprocessed/Mapping/${name}_sorted.bam
+  	samtools sort $workdir/02_Preprocessed/Mapping/${name}.bam -o $workdir/02_Preprocessed/Mapping/${name}_sorted.bam
   	samtools index $workdir/02_Preprocessed/Mapping/${name}_sorted.bam 
   fi
   
   rm $workdir/02_Preprocessed/Mapping/${name}.bam
 done
 
-featureCounts -t exon -C -g gene_id -p -s 2 -T $NUM_PROC_SMALL -a $workdir/01_BulkRNA_preprocessing/Reference/hg38.refGene.gtf -o $workdir/02_Preprocessed/Count/JKT_CRISPR_featurecounts.txt $workdir/02_Preprocessed/Mapping/*_sorted.bam 
+featureCounts -t exon -C -g gene_id -p -s 2 -T $NUM_PROC_SMALL -a /mnt/NASBIOINFO_MN/LALT/BIOINFO/References/Human/Hg38_P14/hg38.refGene.gtf -o /mnt/NASBIOINFO_MN/LALT/BIOINFO/BEX/02_Preprocessed/Count/allbam_featurecounts.txt /mnt/NASBIOINFO_MN/LALT/BIOINFO/BEX/02_Preprocessed/Mapping/*_sorted.bam 
 #param with 2 is library specific, need to be check if launch on other data
